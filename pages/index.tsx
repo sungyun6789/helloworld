4c7b1e8308c0cbf06bb89ee031b93ccf5508c4d3
sungyun <sungyun5423@gmail.com>
2022-03-19 19:15:58 +0900

    feat: json db 연동 및 레이아웃 구현

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,50 @@
+import styled from '@emotion/styled';
 import { NextPage } from 'next';
 
+import Editor from '../components/Editor';
+import database from '../database.json';
+
 const index: NextPage = () => {
-  return <>Home</>;
+  return (
+    <>
+      <Main>
+        <section>
+          {database.map(({ language, value }) => (
+            <Wrapper key={language}>
+              <Language>{language}</Language>
+              <Editor value={value} language={language} readOnly padding={15} />
+            </Wrapper>
+          ))}
+        </section>
+      </Main>
+    </>
+  );
 };
 
 export default index;
+
+const Main = styled.main`
+  display: flex;
+  justify-content: space-between;
+  max-width: 1500px;
+  margin: 0 auto;
+
+  section {
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    margin: 0 auto;
+    flex-wrap: wrap;
+  }
+`;
+
+const Wrapper = styled.article`
+  width: 300px;
+  height: 200px;
+  margin: 50px;
+`;
+
+const Language = styled.p`
+  font-size: 20px;
+  text-align: center;
+`;
